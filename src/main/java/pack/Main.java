adbf1003409ea13ac2cf10eb626f2b4224f2a12f
Annnanke <kirikanna3005@gmail.com>
2022-06-14 11:34:36 -0400

    Final version junit dependencies fixed

--- src/main/java/pack/Main.java
+++ src/main/java/pack/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) throws NoSuchPaddingException, IllegalBlockSizeException,
             NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
 
-        Message sent_msg = new Message(1, 1, "My name is Roman!".getBytes(StandardCharsets.UTF_8));
+        Message sent_msg = new Message(1, 1, "My name is Anna Kirik!".getBytes(StandardCharsets.UTF_8));
         Packet input = new Packet((byte) 1, 42, sent_msg);
 
         byte[] encodedInput = Packet.encodePackage(input);
