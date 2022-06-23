ad7d23c35808f453efd6dcf3d5528631bb89e3b5
Annnanke <kirikanna3005@gmail.com>
2022-06-23 14:57:19 -0400

    Final version

--- src/main/java/pack/Main.java
+++ src/main/java/pack/Main.java
@@ -1,24 +1,15 @@
 package pack;
 
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
 public class Main {
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws NoSuchPaddingException, IllegalBlockSizeException,
-            NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        Data d = new Data();
 
-        Message sent_msg = new Message(1, 1, "My name is Anna Kirik!".getBytes(StandardCharsets.UTF_8));
-        Packet input = new Packet((byte) 1, 42, sent_msg);
-
-        byte[] encodedInput = Packet.encodePackage(input);
-        Packet packet = Packet.decodePackage(encodedInput);
-        String received_msg = new String(packet.message.getMsg(), StandardCharsets.UTF_8);
-        System.out.println(received_msg);
+        for (int i = 0; i < 5; i++) {
+            new Worker(1, d);
+            new Worker(2, d);
+            new Worker(3, d);
+        }
     }
 }
